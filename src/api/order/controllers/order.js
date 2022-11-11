4300a9627cc02a324e53483a7952ac14078e1547
Joelle Rublee <jorubelle@icloud.com>
2022-11-11 09:56:12 -0500

    My first commit

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,50 @@
+// src/api/order/controllers/order.js
+'use strict';
+
+const stripe = require('stripe')("sk_test_51LkBlcKuOke8qQn9HQPcq3SEtJ9lgiX162eHeOJuXiYrTXewPEwyQ4RnRUtbaLSkxneFBeAi0QCXTlPeDWeQ4jfL00n79jcXyP");
+/**
+ *  order controller
+ */
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order.order', ({ strapi }) =>  ({
+  async create(ctx) {
+    const {
+      amount,
+      address,
+      dishes,
+      token,
+    } = ctx.request.body.data;
+    try {
+      // Charge the customer
+      await stripe.charges.create({
+        amount: amount,
+        currency: 'eur',
+        description: `Order ${new Date()} by ${ctx.state.user.id}`,
+        source: token,
+      });
+
+    //    // Register the order in the database
+    //    try {
+    //     const order = await strapi.services.order.create({
+    //       user: ctx.state.user.id,
+    //       address,
+    //       amount,
+    //       dishes,
+    //       postalCode,
+
+    //     });
+
+      // Create the order
+     const entity = await strapi.service('api::order.order').create({ amount, address, dishes, user: ctx.state.user.id });
+     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+     return this.transformResponse(sanitizedEntity);
+    } catch (err) {
+      // return 500 error
+      ctx.response.status = 500;
+      return { error: { message: 'There was a problem creating the charge'}};
+    }
+  }
+}));
+
+
