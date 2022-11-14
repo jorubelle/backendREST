46242497684f0148092dd1119bc88d70edf78c26
Joelle Rublee <jorubelle@icloud.com>
2022-11-14 18:33:30 -0500

    add db config

--- config/database.js
+++ config/database.js
@@ -10,11 +10,11 @@ module.exports = ({ env }) => ({
   },
 });
 
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://jorubelle:<password>@cluster0.m0jhnqd.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://jorubelle:<password>@cluster0.m0jhnqd.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
