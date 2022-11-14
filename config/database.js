a8014464b66d627dc29222b06931962379a40c4f
Joelle Rublee <jorubelle@icloud.com>
2022-11-14 14:39:16 -0500

    changed some packages

--- config/database.js
+++ config/database.js
@@ -9,3 +9,12 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://jorubelle:<password>@cluster0.m0jhnqd.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
