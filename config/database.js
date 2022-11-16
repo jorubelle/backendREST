80d5af9a081548d88d7b2471f5010593ef1a0107
Joelle Rublee <jorubelle@icloud.com>
2022-11-16 17:35:26 -0500

    test to try db

--- config/database.js
+++ config/database.js
@@ -1,37 +1,38 @@
 const path = require('path');
 const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL)
 
 module.exports = ({ env }) => {
   
-  // if(env('NODE_ENV') === 'production') {
-  //   const config = parse(process.env.DATABASE_URL)
-  //   return {
-  //     defaultConnection: 'default',
-  //     connections: {
-  //       default: {
-  //         connector: 'bookshelf',
-  //         settings: {
-  //           client: 'postgres',
-  //           host: config.host,
-  //           port: config.port,
-  //           database: config.database,
-  //           username: config.user,
-  //           password: config.password
-  //         },
-  //         options: {
-  //           ssl: {
-  //             rejectUnauthorized: false
-  //         },
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+  if(env('NODE_ENV') === 'production') {
+    
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password
+          },
+          options: {
+            ssl: {
+              rejectUnauthorized: false
+          },
+          }
+        }
+      }
+    }
+  }
   
   return {
     defaultConnection: 'default',
     connection: {
-      client: 'sqlite',
+      client: 'postgres',
       connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
