8f5244a79d29cbd8cb8320e2a5665a0524a387a6
Joelle Rublee <jorubelle@icloud.com>
2022-11-16 18:10:35 -0500

    added db again

--- config/database.js
+++ config/database.js
@@ -1,48 +1,48 @@
-// const parse = require('pg-connection-string').parse;
+const parse = require('pg-connection-string').parse;
 
-// module.exports = ({ env }) => {
+module.exports = ({ env }) => {
 
-//   if(env('NODE_ENV') === 'production'){
-//     const config = parse(process.env.DATABASE_URL);
-//     return {
-//       defaultConnection: 'default',
-//       connections: {
-//         default: {
-//           //connector: 'bookshelf',
-//           settings: {
-//             client: 'postgres',
-//             host: config.host,
-//             port: config.port,
-//             database: config.database,
-//             username: config.user,
-//             password: config.password,
-//           },
-//           options: {
-//             ssl: {
-//                   rejectUnauthorized: false
-//                 }
-//           },
-//         },
-//       },
-//     }
-//   }
+  if(env('NODE_ENV') === 'production'){
+    const config = parse(process.env.DATABASE_URL);
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          //connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password,
+          },
+          options: {
+            ssl: {
+                  rejectUnauthorized: false
+                }
+          },
+        },
+      },
+    }
+  }
   
-//   return {
-//     defaultConnection: 'default',
-//     connections: {
-//       default: {
-//         //connector: 'bookshelf',
-//         settings: {
-//           client: 'sqlite',
-//           filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//         },
-//         options: {
-//           useNullAsDefault: true,
-//         },
-//       },
-//     },
-//   }
-// };
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        //connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
+    },
+  }
+};
 
 
 
