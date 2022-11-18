1b60c45caa49fe1aff1db3a6ae364af346e8008d
Joelle Rublee <jorubelle@icloud.com>
2022-11-18 10:44:48 -0500

    changed database

--- config/database.js
+++ config/database.js
@@ -1,44 +1,65 @@
-const path = require('path');
-const parse = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => {
-  
-  // if(env('NODE_ENV') === 'production') {
-  //   const config = parse(process.env.DATABASE_URL)
-  //   return {
-  //     defaultConnection: 'default',
-  //     connections: {
-  //       default: {
-  //         connector: 'bookshelf',
-  //         settings: {
-  //           client: 'postgres',
-  //           host: config.host,
-  //           port: config.port,
-  //           database: config.database,
-  //           username: config.user,
-  //           password: config.password
-  //         },
-  //         options: {
-  //           ssl: {
-  //             rejectUnauthorized: false
-  //         },
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-  
-  return {
-    defaultConnection: 'default',
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client: 'sqlite',
-      connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
     useNullAsDefault: true,
   },
-}
-};
+});
+
+
+
+
+
+
+
+
+
+// const path = require('path');
+// const parse = require('pg-connection-string').parse;
+
+// module.exports = ({ env }) => {
+  
+//   // if(env('NODE_ENV') === 'production') {
+//   //   const config = parse(process.env.DATABASE_URL)
+//   //   return {
+//   //     defaultConnection: 'default',
+//   //     connections: {
+//   //       default: {
+//   //         connector: 'bookshelf',
+//   //         settings: {
+//   //           client: 'postgres',
+//   //           host: config.host,
+//   //           port: config.port,
+//   //           database: config.database,
+//   //           username: config.user,
+//   //           password: config.password
+//   //         },
+//   //         options: {
+//   //           ssl: {
+//   //             rejectUnauthorized: false
+//   //         },
+//   //         }
+//   //       }
+//   //     }
+//   //   }
+//   // }
+  
+//   return {
+//     defaultConnection: 'default',
+//     connection: {
+//       client: 'sqlite',
+//       connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// }
+// };
 
 // const { MongoClient } = require('mongodb');
 // const uri = "mongodb+srv://jorubelle:<password>@cluster0.m0jhnqd.mongodb.net/?retryWrites=true&w=majority";
