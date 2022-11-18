d1aad237d5a4395a219e7a5ff0597716ba5a145d
Joelle Rublee <jorubelle@icloud.com>
2022-11-18 10:42:08 -0500

    added long name to provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
             upload: {
-            provider: 'aws-s3',
+            provider: 'strapi-provider-upload-aws-s3',
             providerOptions: {
                 accessKeyId: env('AWS_ACCESS_KEY_ID'),
                 secretAccessKey: env('AWS_ACCESS_SECRET'),
-                region: env('AWS_REGION'),
+                region: env('us-east-1'),
                 params: {
                     Bucket: env('AWS_BUCKET')
                 }
