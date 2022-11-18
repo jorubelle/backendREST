b4a490cb8641662d5f777679eee0b136a679315f
Joelle Rublee <jorubelle@icloud.com>
2022-11-18 14:53:53 -0500

    trouuble shoot

--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,23 @@
 module.exports = ({ env }) => ({
-            upload: {
-            provider: 'strapi-provider-upload-aws-s3',
-            providerOptions: {
-                accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                secretAccessKey: env('AWS_ACCESS_SECRET'),
-                region: env('us-east-1'),
-                params: {
-                    Bucket: env('AWS_BUCKET')
-                }
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-              },
-        }
-    })
+    // ...
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    // ...
+  });
     
